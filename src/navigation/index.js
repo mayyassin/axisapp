11a682118f0a019df03b39f27a7f7fb76d11c6c2
mayyassin <maymay27205@gmail.com>
2023-05-04 16:01:49 +0300

    complete navbar design no border

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,12 +1,23 @@
 import React from "react";
-import { StyleSheet, Button, View, Text } from "react-native";
+import {
+  StyleSheet,
+  Button,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "../screens/Home";
 import AboutUs from "../screens/AboutUs";
 import ChatScreen from "../screens/ChatScreen";
-import chaticon from "../../assets/chaticon.svg";
-import { ChatIcon } from "./styles";
+import chaticon from "../../assets/chaticon.png";
+import axisparrotlogo from "../../assets/AxisParrotLogo.png";
+import axislogofull from "../../assets/axislogofull.png";
+import arrowleft from "../../assets/arrowleft.png";
+import { ChatIcon, LogoIcon, ArrowLeft, HeaderText } from "./styles";
 
 function RootNavigator() {
   const Stack = createNativeStackNavigator();
@@ -17,23 +28,66 @@ function RootNavigator() {
         screenOptions={{
           headerStyle: {
             backgroundColor: "#1ecad4",
+            borderBottomWidth: 5,
+            borderBottomColor: "#000",
           },
         }}
       >
-        <Stack.Screen name=" " component={Home} />
-        <Stack.Screen name="Chat" component={ChatScreen} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ title: null, headerShadowVisible: false }}
+        />
+        <Stack.Screen
+          name="Chat"
+          component={ChatScreen}
+          options={({ navigation }) => ({
+            headerLeft: () => (
+              <TouchableOpacity
+                onPress={() => navigation.navigate("Home")}
+                activeOpacity={0.5}
+                title="chat"
+              >
+                <View>
+                  <HeaderText>_</HeaderText>
+
+                  <LogoIcon source={axislogofull} alt="icon" />
+                </View>
+              </TouchableOpacity>
+            ),
+            title: null,
+          })}
+        />
         <Stack.Screen
           name="AboutUs"
           component={AboutUs}
           options={({ navigation }) => ({
             title: "About Axis",
             headerRight: () => (
-              /* <Button
+              <TouchableOpacity
                 onPress={() => navigation.navigate("Chat")}
-                title="Chat"
-                color="#000000"
-            />*/
-              <ChatIcon source={chaticon} alt="chat" />
+                activeOpacity={0.5}
+                title="chat"
+              >
+                <View>
+                  <HeaderText>_</HeaderText>
+
+                  <ChatIcon source={chaticon} alt="icon" />
+                </View>
+              </TouchableOpacity>
+            ),
+            headerLeft: () => (
+              <TouchableOpacity
+                onPress={() => navigation.navigate("Home")}
+                activeOpacity={0.5}
+                title="chat"
+              >
+                <View>
+                  <HeaderText>_</HeaderText>
+
+                  <ArrowLeft source={arrowleft} alt="icon" />
+                </View>
+              </TouchableOpacity>
             ),
           })}
         />
--- src/navigation/styles.js
+++ src/navigation/styles.js
@@ -1,6 +1,30 @@
 import styled from "styled-components/native";
 
 export const ChatIcon = styled.Image`
-  height: 20%;
-  width: 20%;
+  flex-direction: row;
+  width: 40px;
+  height: 30px;
+  margin-bottom: 5px;
+`;
+
+export const LogoIcon = styled.Image`
+  flex-direction: row;
+  width: 100px;
+  height: 35px;
+  margin-bottom: 10px;
+  //border-radius: 100%;
+  // border-color: #000000;
+  //border-width: 0.5px;
+`;
+
+export const ArrowLeft = styled.Image`
+  flex-direction: row;
+  width: 30px;
+  height: 25px;
+`;
+
+export const HeaderText = styled.Text`
+  font-size: 1px;
+  text-align: center;
+  font-weight: bold;
 `;
