02d92edbb939d2f437ef673659ab235d48619bee
kalmansour <63953899+kalmansour@users.noreply.github.com>
2023-05-21 13:27:26 +0300

    added variables

---
+++ src/variables.js
@@ -0,0 +1,31 @@
+// colors
+export const black = "#1d1111";
+export const white = "#ffffff";
+export const lightGrey = "#e8e7e7";
+export const grey = "#d2d0cf";
+export const darkGrey = "#a39f9f";
+export const textBlack = "#493e3e";
+
+export const primaryblue = "#1ecad3";
+export const darkRed = "#bf3a3a";
+export const lightRed = "#f26c6c";
+export const lightBg = "#feeeed";
+
+export const green = "#00b451";
+export const red = "#ce0e2d";
+export const blue = "#0075c9";
+export const pink = "#e31d93";
+
+// spacing
+export const spacingXs = "0.25em";
+export const spacingSm = "0.5em";
+export const spacingMd = "1em";
+export const spacingLg = "2em";
+export const spacingXl = "3em";
+
+// font
+export const fontXs = "0.875rem";
+export const fontSm = "1rem";
+export const fontMd = "1.25rem";
+export const fontLg = "1.5rem";
+export const fontXl = "2rem";
