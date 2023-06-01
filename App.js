ac0fb1e44aef20093e822221be60015040d537db
kalmansour <63953899+kalmansour@users.noreply.github.com>
2023-06-01 11:25:42 +0300

    added TSRotger fonts

--- App.js
+++ App.js
@@ -1,7 +1,4 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import RootNavigator from "./src/navigation";
-import Home from "./src/screens/Home";
 import {
   useFonts,
   Manjari_400Regular,
@@ -12,6 +9,10 @@ import { ActivityIndicator } from "react-native";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
+    TSRotger_Regular: require("./assets/TSRotger-Regular.ttf"),
+    TSRotger_Bold: require("./assets/TSRotger-Bold.ttf"),
+    TSRotger_Light: require("./assets/TSRotger-Light.ttf"),
+    TSRotger_Black: require("./assets/TSRotger-Black.ttf"),
     Manjari_100Thin,
     Manjari_400Regular,
     Manjari_700Bold,
